33b9d41d64078cc8db3c96b579b280db23eafec3
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-16 22:12:33 +0200

    Define VonKoch.py skeleton

---
+++ src/VonKoch.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import tkinter as tk
+import math
+
+
+class Point: ...
+
+class VonKoch:
+
+    def __init__(self):
+        pass
+
+    def _right(self, x):
+        pass
+
+    def _left(self, x):
+        pass
+
+    def _draw_four_lines(self, side, level):
+        pass
+
+    def display(self):
+        pass
+
+
+def main():
+    VonKoch()
+
+
+if __name__ == "__main__":
+    main()
